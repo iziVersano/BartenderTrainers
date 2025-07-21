2862d3806c91686c9aaf9a950c8b048ce527a68b
Izi versano <iziversano@gmail.com>
2025-07-21 23:55:22 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/client/src",
-      "@assets": "/attached_assets",
+      "@": path.resolve(__dirname, "client/src"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@shared": path.resolve(__dirname, "shared")
     },
   },
+  root: "client",
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     emptyOutDir: true,
   },
-  root: "client",
 });
